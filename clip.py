1f0b198aad9b51ab02712fe6c0c474a66d6135da
Karandeep Singh Nanda <karandeepsinghnanda@Karandeeps-MacBook-Pro.local>
2023-10-04 16:59:54 -0600

    finished clip.py

--- clip.py
+++ clip.py
@@ -16,3 +16,72 @@ class CLIPEmbedding(nn.Module):
         
         x = self.token_embedding()
         
+        x += self.position_embedding
+        
+        return x
+    
+    
+class CLIPLayer(nn.Module):
+    def __init__(self, n_head: int, n_embed:int):
+        super().__init__()
+        
+        self.layernorm_1 = nn.LayerNorm(n_embed)
+        
+        #Self-attention
+        self.attention = SelfAttention(n_head, n_embed)
+        
+        #Pre-FNN norm
+        self.layernorm_2 = nn.LayerNorm(n_embed)
+        
+        #Feedforward Layer
+        self.linear_1 = nn.Linear(n_embed, 4*n_embed)
+        self.linear_2 = nn.Linear(4 * n_embed, n_embed)
+    
+    def forward(self, x):
+        residue = x
+        
+        ## SELF ATTENTION ###
+        x = self.layernorm_1(x)
+        x = self.attention(x, causal_mask = True)
+        
+        x += residue
+        
+        ##FEEDFORWARD Layer
+        
+        residue = x
+        
+        x = self.layernorm_2(x)
+        
+        x   = self.linear_1(x)
+        
+        x = x * torch.sigmoid(1.702 * x)
+        
+        x = self.linear_2(x)
+        
+        x += residue
+        
+        return x
+    
+class CLIP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embedding = CLIPEmbedding(49408, 768, 77)
+        
+        self.layers = nn.ModuleList([
+            CLIPLayer(12,768) for i in range(12)
+        ])        
+        
+        self.layernorm = nn.LayerNorm(768)
+        
+        
+    def forward(self, tokens : torch.LongTensor) -> torch.FloatTensor:
+        tokens = tokens.type(torch.long)
+        
+        
+        state = self.embedding(tokens)
+        for layer in self.layers:
+            state = layer(state)
+        output = self.layernorm(state)
+        
+        return output
+    
