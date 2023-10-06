82ed3835769dfa35da464d1d065e24180c7ee9a3
Karandeep Singh Nanda <karandeepsinghnanda@karandeeps-mbp.lan>
2023-10-05 23:29:28 -0600

    finished model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -12,3 +12,18 @@ def preload_models_from_standard_weights(ckpt_path, device):
     encoder = VAE_Encoder().to(device)
     encoder.load_state_dict(state_dict['encoder'],strict=True)
     
+    decoder = VAE_Decoder().to(device)
+    decoder.load_state_dict(state_dict['decoder'], strict = True)
+    
+    diffusion = Diffusion().to(device)
+    diffusion.load_state_dict(state_dict['diffusion'], strict=True)
+    
+    clip = CLIP().to(device)
+    clip.load_state_dict(state_dict[clip], strict=True)
+    
+    return {
+        'clip' : clip,
+        'encoder' : encoder,
+        'decoder' : decoder,
+        'diffusion' : diffusion,
+    }
